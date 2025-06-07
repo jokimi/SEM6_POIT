138e133ba01cbc00990951c11fe91c2614fb19d5
keikenny <118848006+jokimi@users.noreply.github.com>
2025-06-07 14:04:21 +0300

    Add files via upload

---
+++ ТРВП/4+/asp08/wwwroot/js/Celebrities.js
@@ -0,0 +1,43 @@
+﻿$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+function previewImage(input) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            $('.image-preview').attr('src', e.target.result);
+            $('.image-preview-container').show();
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function confirmDelete(celebrityId, celebrityName) {
+    if (confirm(`Are you sure you want to delete ${celebrityName}?`)) {
+        $.post(`/Celebrities/Delete/${celebrityId}`, function () {
+            window.location.href = '/Celebrities';
+        });
+    }
+}
+
+$(document).ready(function () {
+    $('form').validate({
+        rules: {
+            FullName: "required",
+            Nationality: "required",
+            Photo: {
+                required: true,
+                extension: "jpg|jpeg|png"
+            }
+        },
+        messages: {
+            FullName: "Please enter the celebrity's name",
+            Nationality: "Please select a nationality",
+            Photo: {
+                required: "Please upload a photo",
+                extension: "Only JPG, JPEG or PNG files are allowed"
+            }
+        }
+    });
+});
